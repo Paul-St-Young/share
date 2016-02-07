4073d7bab874559e5b178c459893f7f0b9ea98b1
Paul Young <yyang173@localhost.localdomain>
2016-02-06 19:48:27 -0600

    tweaked fedora script

--- julia-tutorial-pimc/setup-fedora23.sh
+++ julia-tutorial-pimc/setup-fedora23.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
 # Fedora 23 setup script
+# may have to run twice to get everything working
 
 sudo dnf upgrade
 sudo dnf group install "C Development Tools and Libraries" 
 sudo dnf install redhat-rpm-config # solve redhat-hardened-cc1
-sudo pip install jupyter
+
 sudo dnf install freetype-devel # pre-req. for matplotlib
-sudo pip install matplotlib
 sudo dnf install python-devel # pre-req. for numpy
-sudo pip install numpy 
 sudo dnf install lapack-devel # pre-req. for scipy
+
+sudo pip install --upgrade pip # make sure pip is up to date
+sudo pip install jupyter
+sudo pip install matplotlib
+sudo pip install numpy 
 sudo pip install scipy
 sudo dnf install julia
 
@@ -18,3 +22,4 @@ sudo dnf install julia
 echo "Pkg.update()" | julia
 echo "Pkg.add(\"IJulia\")" | julia
 echo "Pkg.add(\"PyPlot\")" | julia
+echo "Pkg.build(\"IJulia\")" | julia # double tap
