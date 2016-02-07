6fa5b95d78c27de28ce2a42eeaf0e4fffd44b123
Paul-St-Young <yyang173@illinois.edu>
2016-02-07 11:13:13 -0600

    grab julia from repo
    also make numpy, scipy install optional

--- julia-tutorial-pimc/setup-fedora23.sh
+++ julia-tutorial-pimc/setup-fedora23.sh
@@ -8,14 +8,17 @@ sudo dnf group install "C Development Tools and Libraries"
 sudo dnf install redhat-rpm-config # solve redhat-hardened-cc1
 
 sudo dnf install freetype-devel # pre-req. for matplotlib
-sudo dnf install python-devel # pre-req. for numpy
-sudo dnf install lapack-devel # pre-req. for scipy
+sudo dnf install python-matplotlib-gtk3 # gtk3 backend - skytux from ask.fedora
+
+# numpy and scipy installations take a while
+#sudo dnf install python-devel # pre-req. for numpy
+#sudo dnf install lapack-devel # pre-req. for scipy
 
 sudo pip install --upgrade pip # make sure pip is up to date
-sudo pip install jupyter
-sudo pip install matplotlib
-sudo pip install numpy 
-sudo pip install scipy
+sudo pip install jupyter matplotlib numpy scipy julia
+
+# install julia
+sudo dnf copr enable nalimilan/julia-nightlies
 sudo dnf install julia
 
 # install "IJulia" and "PyPlot" in Julia
